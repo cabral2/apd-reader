5276366a7857579a1948f1dea4961e421232219a
Iury Zanonni <izanonni@gmail.com>
2020-10-30 00:26:37 -0300

    Desenvolvida funções para push na pilha e retornar o "caminho" de uma transição. Remoção do codigo "Ctrl + D".

--- logic.js
+++ logic.js
@@ -1,20 +1,32 @@
-const readline = require("readline");
-
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
-
-process.stdin.on("keypress", (str, key) => {
-  console.log(key);
-  if (key && key.sequence === "\u0004") process.exit();
-});
-
 let apd;
 let word;
 let stack;
 let currentState;
 
-const handleInput = (input) => {};
+const handleInput = (input) => {
+  let letter = input;
+};
 
+//Método para inserir os simbolos na pilha na ordem correta
+const inputStack = (input) => {
+  if(input.length>1){
+    for(let i = input.length - 1; i>=0; i--){
+      stack.push(input[i]);
+    }
+  }
+}
+
+//Função que retorna o 'caminho' de um estado atual para um estado futuro através do valor da trnasição
+const handlePath = (input) =>{
+  for(let i = 0; i< apd.partialFunctions.length; i++){
+    if(apd.partialFunctions[i].currentState == currentState && apd.partialFunctions[i].entry == input ){      
+      inputStack(apd.partialFunctions[i].stackIn);   //Apenas teste do push
+      return apd.partialFunctions[i];
+    }
+  }
+}
+
+// Função que executa toda a logica do APD
 const run = (apdInput, wordInput) => {
   apd = apdInput;
   word = wordInput;
@@ -22,8 +34,13 @@ const run = (apdInput, wordInput) => {
   currentState = apd.initialState;
 
   for (let i = 0; i < word.length; i++) {
-    handleInput(word[i]);
+    //handleInput(word[i]);
+    handlePath(word[i]);
   }
+  
+  console.log(stack);
+
+
 };
 
 module.exports = {
